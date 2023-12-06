1ff9c5024bf8fdec3c4688f05064d25ddc899672
marcelanapoli <marcelanapoli100@gmail.com>
2023-12-06 19:16:09 -0300

    1er commit

---
+++ inserts.sql
@@ -0,0 +1,8 @@
+INSERT INTO producto VALUES(1,"Cuerda Kirschbaum Pro Line II", 70300, 20,"./images/Cueda Proline II.jpg");
+INSERT INTO producto VALUES(2,"Cuerda Kirschbaum Max Power", 76300, 20,"./images/Cuerda Max Power.jpg");
+INSERT INTO producto VALUES(3,"Cuerda Kirschbaum Competition", 56000, 20,"./images/Cuerda Competition.jpg");
+INSERT INTO producto VALUES(4,"Pelotas Head Tour", 6400, 20,"./images/Pelotas Head Tour.jpg");
+INSERT INTO producto VALUES(5,"Pelotas Head Pro", 6500, 20,"./images/Pelotas Head Pro.jpg");
+INSERT INTO producto VALUES(6,"Pelotas Head Championship", 6200, 20,"./images/Pelotas Head Championship.jpg");
+INSERT INTO producto VALUES(7,"Cubregrip Head Prime Pro", 9200, 20,"./images/Cubre Head Prime Pro.jpg");
+INSERT INTO producto VALUES(8,"Cubregrip Head Prime Tour", 11000, 20,"./images/Cubre Head Prime Tour.jpg");
